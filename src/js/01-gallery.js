5a6ae2eb8c2c50b64b882921f192a9c125657633
MateuszFrankowski <mateuszfrankowski91@gmail.com>
2022-11-10 22:29:29 +0100

    HM 1 and 2(80%)

---
+++ src/js/01-gallery.js
@@ -0,0 +1,34 @@
+// Add imports above this line
+import { galleryItems } from './gallery-items';
+// Change code below this line
+
+console.log(galleryItems);
+
+// Opisany w dokumentacji
+import SimpleLightbox from 'simplelightbox';
+// Dodatkowy import stylów
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const gallery = document.querySelector('.gallery');
+const markup = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      ` <a class="gallery__item" href="${original}">
+        <img
+        class="gallery__image"
+        src="${preview}"        
+        alt="${description}"
+     
+      />
+      </a>`
+  )
+  .join('');
+
+gallery.insertAdjacentHTML('afterbegin', markup);
+let lightbox = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionSelector: 'img',
+  captionDelay: 250,
+  captionPosition: 'bottom',
+  captionsData: 'alt',
+});
