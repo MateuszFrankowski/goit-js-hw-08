d19c55d9785ca4f24183ff37d5dfd6bb8f082af0
MateuszFrankowski <mateuszfrankowski91@gmail.com>
2022-11-13 22:32:47 +0100

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,40 +1,34 @@
 const form = document.querySelector('form');
 import throttle from 'lodash.throttle';
-
+const saveToLocal = () => {
+  if (data.message !== '' || data.email !== '') {
+    localStorage.setItem('feedback-form-state', JSON.stringify(data));
+  }
+};
+const data = {};
 const handleInput = event => {
-  const data = {};
-  //nie działa przy:  trailing: true i pisaniu znaków szybciej jak co 500ms
+  console.log('pisze');
   const {
     elements: { email, message },
   } = event.currentTarget;
   data.email = email.value;
   data.message = message.value;
-  // działa
-  // data.email = form.elements.email.value;
-  // data.message = form.elements.message.value;
-
-  if (data.message !== '' || data.email !== '') {
-    localStorage.setItem('feedback-form-state', JSON.stringify(data));
-  }
 };
 
-form.addEventListener(
-  'input',
-  throttle(handleInput, 500, {
-    trailing: true,
-  })
-);
+form.addEventListener('input', handleInput);
 const formState = localStorage.getItem('feedback-form-state');
-const parsedFormState = JSON.parse(formState);
-if (parsedFormState) {
+if (formState) {
+  const parsedFormState = JSON.parse(formState);
+
   form.elements.email.value = parsedFormState.email;
   form.elements.message.value = parsedFormState.message;
   console.log('updated');
 }
+form.addEventListener('input', throttle(saveToLocal, 500));
 
 const handleSubmit = event => {
   event.preventDefault();
-  const data = {};
+
   const {
     elements: { email, message },
   } = event.currentTarget;
@@ -42,6 +36,6 @@ const handleSubmit = event => {
   data.message = message.value;
   console.log(`Email: ${data.email}, Message: ${data.message}`);
   event.currentTarget.reset();
-  localStorage.clear(); // localStorage.removeItem('feedback-form-state');
+  localStorage.clear();
 };
 form.addEventListener('submit', handleSubmit);
